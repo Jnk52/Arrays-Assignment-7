eace2920bf0754cb8ef6dd1938890a250dbee1a5
Jnk52 <65060353+Jnk52@users.noreply.github.com>
2020-05-13 09:21:08 -0700

    Arrays-Assignment-7

---
+++ Day7/PrimefactorizationInArray.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+echo "Enter the number"
+read num
+array=(num)
+
+for (( i=2 ; i<=num ; i++ ))
+
+do
+        while [ $((num % $i)) -eq 0 ]
+        do
+                echo $i
+                num=$((num/$i))
+
+        done
+done
